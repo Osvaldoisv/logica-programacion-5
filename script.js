453283a73c91d7a7b6981f4b8a5dd04db1c4b4ba
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-01-25 16:22:28 -0300

    añadir comentarios js

--- script.js
+++ script.js
@@ -9,23 +9,19 @@ function borrarLista(lista){
 }
 
 function setRatio(){
-    ratio = document.getElementById("Ratio").value;
-    console.log(`el valor es ${ratio}`);
+    ratio = document.getElementById("Ratio").value;                     // Recibimos el rango
     let numeroAleatorio = Math.floor((Math.random(1)*(ratio))+1);
-    console.log(`el numero aleatorio es ${numeroAleatorio}`)
+    console.log(`el numero aleatorio es ${numeroAleatorio}`)            // Mostramos internamente el numero correcto
     return numeroAleatorio;
 }
 
 function borrarInput() {
-    // Obtener el elemento input por su ID
     var input = document.getElementById("Entrada");
-    
-    // Establecer el valor del input como una cadena vacía
-    input.value = "";
+    input.value = ""; // Establecer el valor del input como una cadena vacía
 }
 console.log(numeroAleatorio);
 
-function visualToggle(){
+function visualToggle(){                // Esto es para mostrarle al USER el modo en el que es´ta (Seleccionar numero o rango)
     let estadoBoton = document.getElementById("Estado").getAttribute("aria-pressed");
     let nombreBoton = document.getElementById("Estado");
 
@@ -37,18 +33,18 @@ function visualToggle(){
 }
 
 
-function apretarBoton(){
+function apretarBoton(){                            
 
     let entrada = document.getElementById("Entrada").value;
     let intentos = document.getElementById("Intentos");
-    let estadoBoton = document.getElementById("Estado").ariaPressed;
+    let estadoBoton = document.getElementById("Estado").ariaPressed; // Valor del toggle
     intentos.innerHTML = "Intentos: 0";
 
-    console.log(estadoBoton);
-    if (estadoBoton == true){
+    console.log(estadoBoton);                       
+    if (estadoBoton == true){       // Si seleccionar NUMERO está apretado, el numero correcto será el NUMERO del input
         numeroAleatorio = document.getElementById("Ratio").value;
 
-    } else {
+    } else {                        // Si no, generámos un numero aleatorio dentro de un rango establecido
         numeroAleatorio = setRatio();
     }
 
@@ -59,21 +55,21 @@ function apretarBoton(){
     nuevaLista.appendChild(elementoNuevo);                  // Añadimos elemento hijo a la lista
     listaActual.appendChild(nuevaLista);
 
-    var ultimoElemento = listaActual.lastElementChild;
+    var ultimoElemento = listaActual.lastElementChild;          // Para llamar el último elemento
 
-    listaVacia.push(parseInt(ultimoElemento.textContent));
-    intentos_suma = listaVacia.length;
+    listaVacia.push(parseInt(ultimoElemento.textContent));  // Lo insertamos en una lista
+    intentos_suma = listaVacia.length;                      // Calculamos el largo de la lista
 
-    if (parseInt(ultimoElemento.textContent) !== numeroAleatorio){
+    if (parseInt(ultimoElemento.textContent) !== numeroAleatorio){      
         ultimoElemento.classList.add('ultimo-elemento-malo')
         intentos.innerHTML = 'intentos: ' + intentos_suma;
-        borrarInput();
+        borrarInput();                                              // Borramos el input rapidamente para seguir ingresando numeros
     } else{
         ultimoElemento.classList.add('ultimo-elemento-bueno')
         intentos.innerHTML = 'intentos: ' + intentos_suma;
-        
-        setTimeout(function() {
-            if (window.confirm("¡Felicidades!")) {
+                                                                    
+        setTimeout(function() {                             // Al conseguir ganar, aparece un mensaje en pantalla
+            if (window.confirm("¡Felicidades!")) {          // Si aceptamos, se reinicia el juego
                 borrarLista(listaActual);
                 intentos.innerHTML = "Intentos: 0";
                 intentos_suma = 0;
@@ -84,9 +80,8 @@ function apretarBoton(){
     }
 }
 
-function detectarEnter(event) {
+function detectarEnter(event) {                     // Podemos dar click en boton PROBAR o con la tecla ENTER
     if (event.key === "Enter") {
-      // Llama a la función que quieres ejecutar cuando se presiona Enter
       apretarBoton();
     }
 }
